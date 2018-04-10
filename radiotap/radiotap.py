82bb62c63353fcfc2437e4499e126bb87bd6ab39
techge <techge+github@posteo.net>
2018-04-10 13:59:07 +0200

    README added, frag number fixed (upstream)

--- radiotap/radiotap.py
+++ radiotap/radiotap.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
-# parse radiotap fields from pcap buffers into a dictionary
-#
+
+'''
+Copyright (c) 2017-2018     Alexander Paetzelt <github ät techge de>
+Copyright (c) 2012-2016     Bob Copeland <me@bobcopeland.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
+
 # example:
-# >>> import radiotap as r, pcap
-# >>> pc = pcap.pcap(name='foo.pcap')
-# >>> tstamp, pkt = pc[0]
-# >>> off, radiotap = r.radiotap_parse(pkt)
-# >>> off, mac = r.ieee80211_parse(pkt, off)
+# >>> from radiotap import radiotap_parse, ieee80211_parse
+# >>> off, radiotap = r.radiotap_parse(packet_block)
+# >>> off, mac = r.ieee80211_parse(packet_block, off)
+
 import struct
-
 from vht import *
-
 
 mcs_rate_table = [
     (6.50, 7.20, 13.50, 15.00),
@@ -437,16 +450,13 @@ def ieee80211_parse(packet, offset):
     fc, duration, addr1 = \
         struct.unpack_from(hdr_fmt, packet, offset)
 
-    fc_detail= {
+    offset += hdr_len
+
+    mac = {
+        'fc': fc,
         'protocol': int(0x3 & fc),
         'type': frame_type[(0x3 & fc>>2)],
         'subtype': frame_subtype[(0x30 & fc<<2) | (0xf & fc>>4)],
-    }
-
-    offset += hdr_len
-    mac = {
-        'fc': fc,
-        'fc_detail': fc_detail,
         'duration': duration * .001024,
         'addr1': macstr(addr1),
     }
@@ -464,7 +474,7 @@ def ieee80211_parse(packet, offset):
             'addr2': macstr(addr2),
             'ba_ctrl': ba_ctrl,
             'ba_ssc': ba_ssc,
-            'ba_bitmap': ba_bitmap
+            'ba_bitmap': ba_bitmap.hex()
         })
         return offset, mac
 
@@ -480,7 +490,7 @@ def ieee80211_parse(packet, offset):
         'addr2': macstr(addr2),
         'addr3': macstr(addr3),
         'seq': seq >> 4,
-        'frag': seq & 3
+        'frag': seq & 0x0f
     })
 
     if is_qos(mac):
